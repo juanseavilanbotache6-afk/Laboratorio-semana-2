a2d895802a2a2ed50bc7c65bdf5a789b014435a2
Santiago Pinzon Coronado <santiagopc0610@hotmail.com>
2025-08-30 13:15:34 -0500

    cambio

--- servidor.py
+++ servidor.py
@@ -1,15 +1,16 @@
-import socket
+import socket #cliente-servidor
 
 HOST = '127.0.0.1'  # Dirección local
 PORT = 65432        # Puerto no reservado
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
     servidor.bind((HOST, PORT))
-    servidor.listen()
+    servidor.listen() #esperando que un cliente intente conectarse
     print(f"Servidor escuchando en {HOST}:{PORT}...")
-
+    # socket.AF_INET-indica que se usará IPv4
+    # socket.SOCK_STREAM-el socket usará TCP (conexión confiable)
     
-    conn, addr = servidor.accept()
+    conn, addr = servidor.accept() #addr contiene la dirección IP y puerto del cliente que se conectó
     with conn:
         print(f"Conectado por {addr}")
         while True:
